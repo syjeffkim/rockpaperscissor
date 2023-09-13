9300238440d239b16141e41f41ec57390aabd1ca
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2023-09-13 08:25:56 -0700

    practice commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 100 + 1);
+    console.log(number);
+ /*   if (number < 33) {
+        console.log("rock"); 
+    }
+    if (66 > number > 33) {
+    console.log("scissor");
+    }
+    if (number > 66) {
+    console.log("paper");
+    }
+*/
+    if (number < 33) {
+        console.log("rock"); ;
+      } else if (number < 66) {
+        console.log("scissor");
+      } else {
+        console.log("paper");
+      }
+}
+
+/* 
+input rock, paper, scissor here
+give out put, "win" or "lose" (determined by player choice)
+*/
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+getComputerChoice();
