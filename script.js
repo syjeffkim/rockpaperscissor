9db581a3741fbd3c4bc2664283895306f0d3629b
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2023-09-13 09:04:18 -0700

    Add computerChoice function and choices array
    Original math function did not equally distribute choice change to 1/3 for each.
    The computerChoice function fixes that and the choice array will help link
    the numbers to the strings we want for the output.

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
+const choices = ["rock", "paper", "scissor"]
 
-function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100 + 1);
-    console.log(number);
- /*   if (number < 33) {
-        console.log("rock"); 
-    }
-    if (66 > number > 33) {
-    console.log("scissor");
-    }
-    if (number > 66) {
-    console.log("paper");
-    }
-*/
-    if (number < 33) {
-        console.log("rock"); ;
-      } else if (number < 66) {
-        console.log("scissor");
-      } else {
-        console.log("paper");
-      }
+function computerChoice() {
+  return [Math.floor(Math.random() * choices.length)]
+
+  /* allows you to pick a random number from 0 to > 1 and multiply it by 3 while rounding down. Forces number to be 0, 1, or 2 which will
+  be 0 = rock, 1 = paper, 2 = scissor */
+
 }
 
-/* 
-input rock, paper, scissor here
-give out put, "win" or "lose" (determined by player choice)
-*/
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-
-getComputerChoice();
-
-// 9/13 practice git commit 2 //
-// 9/13 practice git commit last round//
+console.log(computerChoice());
